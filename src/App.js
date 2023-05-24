a9bf5603733119062305f26da7ba82306296c907
Rima Akter <rimawahid3@gmail.com>
2023-05-24 12:33:26 +0600

    login registration form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
 import Youtube from './components/Youtube';
 import FormikContainer from './components/reusableFormikControls/FormikContainer';
 
 function App() {
   return (
     <div className="App">
-      <FormikContainer/>
+      <RegistrationForm/>
+      {/* <LoginForm/> */}
+      {/* <FormikContainer/> */}
       {/* <Youtube/> */}
     </div>
   );
